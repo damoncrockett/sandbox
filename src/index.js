e7b2e84058c1f19b254ca51a5258d3ac2fffc0c9
damoncrockett <damoncrockett@hotmail.com>
2019-01-12 03:43:19 -0500

    rm lodash
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import _ from 'lodash';
-
-function component() {
-  let element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-  }
-
-document.body.appendChild(component());
+console.log("hello");
